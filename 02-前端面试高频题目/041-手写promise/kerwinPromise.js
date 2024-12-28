b340e29dd6ee2c180576bda67a136d52b17a78fb
ltpbje <19836788252@163.com>
2024-12-28 17:05:54 +0800

    037-手写Promise-1

--- 02-前端面试高频题目/041-手写promise/kerwinPromise.js
+++ 02-前端面试高频题目/041-手写promise/kerwinPromise.js
@@ -1,15 +1,26 @@
 function KerwinPromise(executor) {
     this.status = 'pending';
     this.result = undefined;
+    this.cb = [];
     var _this = this;
     function reslove(res) {
+        if (_this.status !== 'pending') return;
         _this.status = 'fulfilled';
         _this.result = res;
+        _this.cb.forEach(item => {
+            item.successCB && item.successCB(res);
+        });
+
         // console.log('reslove');
     }
-    function reject() {
+    function reject(res) {
+        if (_this.status !== 'pending') return;
         _this.status = 'rejected';
         _this.result = res;
+        _this.cb.forEach(item => {
+            item.failCB && item.failCB(res);
+        });
+
     }
     executor(reslove, reject);
 }
@@ -17,7 +28,11 @@ function KerwinPromise(executor) {
 KerwinPromise.prototype.then = function (successCB, failCB) {
     if (this.status === 'fulfilled') {
         successCB && successCB(this.result);
-    } if (this.status === 'rejected') {
-        failCB && (this.result);
+    }
+    if (this.status === 'rejected') {
+        failCB && failCB(this.result);
+    }
+    if (this.status === 'pending') {
+        this.cb.push({ successCB, failCB });
     }
 };
