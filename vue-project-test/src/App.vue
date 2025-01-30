92144c81c5cfd38549123fd1a84f1c281bb65335
ltpbje <19836788252@163.com>
2025-01-30 18:29:12 +0800

    vue3学习

---
+++ vue-project-test/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref, useTemplateRef, onMounted } from 'vue';
+
+const list = ref([1, 2, 3]);
+
+// const itemRefs = useTemplateRef('items');
+const items = ref([]);
+
+onMounted(() => {
+  // console.log(itemRefs.value.map(i => i));
+  console.log(items.value);
+
+});
+</script>
+
+<template>
+  <ul>
+    <li v-for="item in list" ref="items">
+      {{ item }}
+    </li>
+  </ul>
+</template>
