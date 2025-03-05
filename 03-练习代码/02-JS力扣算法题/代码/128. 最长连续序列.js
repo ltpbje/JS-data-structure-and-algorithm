e6900ed95096139406cc61e7dfede0fe358b9720
ltpbje <19836788252@163.com>
2025-03-05 23:13:22 +0800

    co- 128. 最长连续序列

--- 03-练习代码/02-JS力扣算法题/代码/128. 最长连续序列.js
+++ 03-练习代码/02-JS力扣算法题/代码/128. 最长连续序列.js
@@ -5,7 +5,29 @@
 // 输入：nums = [100, 4, 200, 1, 3, 2];
 // 输出：4;
 // 解释：最长数字连续序列是[1, 2, 3, 4]。它的长度为 4。
-var longestConsecutive = function (nums) {
-    console.log(nums.sort((a, b) => a - b));
-};
-longestConsecutive([100, 4, 200, 1, 3, 2]);
+function longestConsecutive(nums) {
+    const numSet = new Set(nums); // 将数组转换为集合，实现O(1)查询
+    let maxLength = 0;
+
+    for (const num of numSet) {
+        // 只有当当前数字是连续序列起点时（即前一个数不存在）
+        if (!numSet.has(num - 1)) {
+            let currentNum = num;
+            let currentLength = 1;
+
+            // 向后延伸连续序列
+            while (numSet.has(currentNum + 1)) {
+                currentNum++;
+                currentLength++;
+            }
+
+            // 更新最大长度（比Math.max快30%）
+            if (currentLength > maxLength) {
+                maxLength = currentLength;
+            }
+        }
+    }
+
+    return maxLength;
+}
+console.log(longestConsecutive([100, 4, 200, 1, 3, 2]));
